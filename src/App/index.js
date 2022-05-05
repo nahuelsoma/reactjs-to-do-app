2fd713b139f627f251632dccfe48ff22641914af
Nahuel Soma <soma.nahuel@gmail.com>
2022-05-05 11:48:18 -0300

    Se crea un custom react hook para almacenar informacion en local storage

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,36 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
-  { text: "Task 1", completed: true },
-  { text: "Task 2", completed: true },
-  { text: "Task 3", completed: false },
-];
+// const defaultTodos = [
+//   { text: "Task 1", completed: true },
+//   { text: "Task 2", completed: true },
+//   { text: "Task 3", completed: false },
+// ];
+
+function useLocalStorage(itemName, initialValue) {
+  const localStorageItem = localStorage.getItem(itemName);
+  let parsedItem;
+
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = initialValue;
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);
+
+  const saveItem = (newItem) => {
+    const stringifiedItem = JSON.stringify(newItem);
+    localStorage.setItem(itemName, stringifiedItem);
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
 
 function App() {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter((todo) => !!todo.completed).length;
@@ -36,14 +58,14 @@ function App() {
       newTodos[todoIndex].completed = false;
     }
 
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   return (
